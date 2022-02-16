8c147fd9fa0e577f956415933d6a85984d10175a
ha6icht <rolf.karlen@gmail.com>
2022-02-16 17:29:08 +0100

    First commit

---
+++ changeLanguage.js
@@ -0,0 +1,4 @@
+const language = navigator.language;
+
+if(language === 'de' || language ==='de-DE' || language === 'de-CH' || language === 'de-LI' || language === 'de-AT') console.log(language);
+else console.log(language, 'I\'m switching to English!');
