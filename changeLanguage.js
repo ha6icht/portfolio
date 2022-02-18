1f88bf9096352efb82a97042b94a7b24c0e66fcc
ha6icht <rolf.karlen@gmail.com>
2022-02-18 20:37:31 +0100

    Delete changeLanguage.js

--- changeLanguage.js
+++
@@ -1,4 +0,0 @@
-const language = navigator.language;
-
-if(language === 'de' || language ==='de-DE' || language === 'de-CH' || language === 'de-LI' || language === 'de-AT') console.log(language);
-else console.log(language, 'I\'m switching to English!');
