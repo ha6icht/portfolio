3b8ed2fc0d5e5196254a0afb0ffafdc709838942
ha6icht <rolf.karlen@gmail.com>
2022-03-19 16:39:42 +0100

    Change hex code to material icon name

--- main.js
+++ main.js
@@ -9,18 +9,18 @@ const changeTheme = () => {
         mode.href = 'basic.css';//'./enlightenment.css';
         if(bodyId === 'index') photo.src = './images/ich_balmkirchli_adjusted.jpg';
         displayMode.title = 'Wechseln zu Dark Theme';
-        displayMode.innerHTML = '&#9728;';
+        displayMode.innerHTML = 'light_mode';
     } else if(localStorage.getItem('theme') === 'dark'){
         mode.href = './dark.css';
         if(bodyId === 'index') photo.src = './images/balmkirchli_adjusted.jpg';
         displayMode.title = 'Wechseln zu Basic Theme';
-        displayMode.innerHTML = '&#9680;'; 
+        displayMode.innerHTML = 'dark_mode'; 
     } else {
         localStorage.setItem('theme', 'basic');
         mode.href = './basic.css';
         if(bodyId === 'index') photo.src = './images/ich_balmkirchli_adjusted.jpg';
         displayMode.title = 'Wechseln zu Dark Theme';
-        displayMode.innerHTML = '&#9728;';
+        displayMode.innerHTML = 'light_mode';
     }
 }
 
@@ -32,18 +32,18 @@ displayMode.addEventListener('click', () => {
         mode.href = './dark.css';
         if(bodyId === 'index') photo.src = './images/balmkirchli_adjusted.jpg';
         displayMode.title = 'Wechseln zu Basic Theme';
-        displayMode.innerHTML = '&#9680;'; 
+        displayMode.innerHTML = 'dark_mode'; 
     } else if(localStorage.getItem('theme') === 'dark'){
         localStorage.setItem('theme', 'basic');
         mode.href = './basic.css';
         if(bodyId === 'index') photo.src = './images/ich_balmkirchli_adjusted.jpg';
         displayMode.title = 'Wechseln zu Dark Theme';
-        displayMode.innerHTML = '&#9728;';
+        displayMode.innerHTML = 'light_mode';
     } else {
         localStorage.setItem('theme', 'basic');
         mode.href = './basic.css';
         if(bodyId === 'index') photo.src = './images/ich_balmkirchli_adjusted.jpg';
         displayMode.title = 'Wechseln zu Dark Theme';
-        displayMode.innerHTML = '&#9728;';
+        displayMode.innerHTML = 'light_mode';
     }
 });
