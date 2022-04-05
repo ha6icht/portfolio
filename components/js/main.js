1485ea65ee4d05db810f938b0f8ea46a94ab6762
ha6icht <rolf.karlen@gmail.com>
2022-04-05 14:57:52 +0200

    Change url to picture

--- components/js/main.js
+++ components/js/main.js
@@ -7,18 +7,18 @@ const changeTheme = () => {
     //console.log('Inside changeTheme()')
     if(localStorage.getItem('theme') === 'basic'){
         mode.href = './components/css/basic.css';//'./enlightenment.css';
-        if(bodyId === 'index') photo.src = './images/ich_balmkirchli_adjusted.jpg';
+        if(bodyId === 'index') photo.src = './components/ich_balmkirchli_adjusted.jpg';
         displayMode.title = 'Wechseln zu Dark Theme';
         displayMode.innerHTML = 'light_mode';
     } else if(localStorage.getItem('theme') === 'dark'){
         mode.href = './components/css/dark.css';
-        if(bodyId === 'index') photo.src = './images/balmkirchli_adjusted.jpg';
+        if(bodyId === 'index') photo.src = './components/images/balmkirchli_adjusted.jpg';
         displayMode.title = 'Wechseln zu Basic Theme';
         displayMode.innerHTML = 'dark_mode'; 
     } else {
         localStorage.setItem('theme', 'basic');
         mode.href = './components/css/basic.css';
-        if(bodyId === 'index') photo.src = './images/ich_balmkirchli_adjusted.jpg';
+        if(bodyId === 'index') photo.src = './components/images/ich_balmkirchli_adjusted.jpg';
         displayMode.title = 'Wechseln zu Dark Theme';
         displayMode.innerHTML = 'light_mode';
     }
@@ -30,19 +30,19 @@ displayMode.addEventListener('click', () => {
     if(localStorage.getItem('theme') === 'basic'){
         localStorage.setItem('theme', 'dark');
         mode.href = './components/css/dark.css';
-        if(bodyId === 'index') photo.src = './images/balmkirchli_adjusted.jpg';
+        if(bodyId === 'index') photo.src = './components/images/balmkirchli_adjusted.jpg';
         displayMode.title = 'Wechseln zu Basic Theme';
         displayMode.innerHTML = 'dark_mode'; 
     } else if(localStorage.getItem('theme') === 'dark'){
         localStorage.setItem('theme', 'basic');
         mode.href = './components/css/basic.css';
-        if(bodyId === 'index') photo.src = './images/ich_balmkirchli_adjusted.jpg';
+        if(bodyId === 'index') photo.src = './components/images/ich_balmkirchli_adjusted.jpg';
         displayMode.title = 'Wechseln zu Dark Theme';
         displayMode.innerHTML = 'light_mode';
     } else {
         localStorage.setItem('theme', 'basic');
         mode.href = './components/css/basic.css';
-        if(bodyId === 'index') photo.src = './images/ich_balmkirchli_adjusted.jpg';
+        if(bodyId === 'index') photo.src = './components/images/ich_balmkirchli_adjusted.jpg';
         displayMode.title = 'Wechseln zu Dark Theme';
         displayMode.innerHTML = 'light_mode';
     }
