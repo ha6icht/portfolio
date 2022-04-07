afb906fbe44d0c20fd495adc69b5567a03680109
ha6icht <rolf.karlen@gmail.com>
2022-04-07 10:11:32 +0200

    Change import path to headerObj.js

--- components/js/main.js
+++ components/js/main.js
@@ -1,4 +1,4 @@
-import header from '../languagePacks/language.js';
+import header from '../languagePacks/headerObj.js';
 
 const displayMode = document.getElementById('display-mode');
 const mode = document.getElementById('mode');
